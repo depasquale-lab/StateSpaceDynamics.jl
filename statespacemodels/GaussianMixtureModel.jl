9ffae61563d187968312f2dcaf9faf514ea8e283
Ryan Senne <50930199+rsenne@users.noreply.github.com>
2023-09-23 13:42:42 -0400

    Add project structure

---
+++ statespacemodels/GaussianMixtureModel.jl
@@ -0,0 +1,24 @@
+module ssm
+
+using Random
+using Statistics
+using LinearAlgebra
+
+export GMM, fit!
+
+mutable struct GMM
+    k_means::Int
+    means::Matrix{Float64}
+    covariances::Array{Matrix{Float64}, 1}
+    weights::Vector{Float64}
+end
+
+"""Initialize a GMM and create initial parameters and return a GMM struct."""
+function GMM(k_means::Int, data_dim::Int)
+    means = randn(data_dim, k_means)
+    covariances = [I(data_dim) for _ = 1:k_means]
+    weights = ones(k_means) ./ k_means
+    return GMM(k_means, means, covariances, weights)
+end
+
+end #module 
