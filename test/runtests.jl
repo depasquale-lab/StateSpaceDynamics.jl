09ff93da8529b1c6484d73da9732c9700249ab34
Ryan Senne <50930199+rsenne@users.noreply.github.com>
2023-10-23 19:47:32 -0400

    Update Regression.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,9 +10,7 @@ function test_GMM_constructor()
     k_means = 3
     data_dim = 2
     data = randn(100, data_dim)
-    
     gmm = GMM(k_means, data_dim, data)
-    
     @test gmm.k_means == k_means
     @test size(gmm.μ_k) == (data_dim, k_means)
     @test length(gmm.Σ_k) == k_means
@@ -28,13 +26,10 @@ function testGMM_EStep()
     data_dim = 2
     data = randn(10, data_dim)
     gmm = GMM(k_means, data_dim, data)
-
     # Run EStep
-    γ = EStep!(gmm, data)
-
+    γ = MixtureModels.EStep!(gmm, data)
     # Check dimensions
     @test size(γ) == (10, k_means)
-
     # Check if the row sums are close to 1 (since they represent probabilities)
     @test all(isapprox(sum(γ, dims=2), 1.0; atol=1e-6))
 end
@@ -45,10 +40,10 @@ function testGMM_MStep()
     data_dim = 2
     data = randn(10, data_dim)
     gmm = GMM(k_means, data_dim, data)
-    γ = EStep!(gmm, data)
+    γ = MixtureModels.EStep!(gmm, data)
 
     # Run MStep
-    MStep!(gmm, data, γ)
+    MixtureModels.MStep!(gmm, data, γ)
 
     # Check dimensions of updated μ and Σ
     @test size(gmm.μ_k) == (data_dim, k_means)
@@ -93,11 +88,14 @@ function test_log_likelihood()
     @test ll < 0.0
 end
 
-test_GMM_constructor()
-testGMM_EStep()
-testGMM_MStep()
-testGMM_fit()
-test_log_likelihood()
+@testset "MixtureModels.jl Tests" begin
+    test_GMM_constructor()
+    testGMM_EStep()
+    testGMM_MStep()
+    testGMM_fit()
+    test_log_likelihood()
+end
+
 
 """
 Tests for HiddenMarkovModels.jl
