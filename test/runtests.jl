e898a22819b2ffd48704498c4c40b28890b4cb98
Ryan Senne <50930199+rsenne@users.noreply.github.com>
2024-05-24 15:57:17 -0400

    Add ppca tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -932,13 +932,14 @@ function test_PPCA_with_params()
     X = randn(100, 3)
     μ = mean(X, dims=1)
     # create PPCA object
-    ppca = ProbabilisticPCA(W, σ², μ, 2, 3)
+    ppca = ProbabilisticPCA(W, σ², μ, 2, 3, Matrix{Float64}(undef, 0, 0))
     # Check if parameters are set correctly
     @test ppca.W === W
     @test ppca.σ² === σ²
     @test ppca.μ === μ
     @test ppca.D === 3
     @test ppca.K === 2
+    @test isempty(ppca.z)
 end
 
 function test_PPCA_without_params()
@@ -950,11 +951,56 @@ function test_PPCA_without_params()
     @test isempty(ppca.μ)
     @test ppca.D == 3
     @test ppca.K == 2
+    @test isempty(ppca.z)
+end
+
+function test_PPCA_E_and_M_Step()
+    # create ppca object
+    ppca = ProbabilisticPCA(;K=2, D=3)
+    # create data
+    X = randn(100, 3)
+    # assign μ, normally fit! does this
+    μ = mean(X, dims=1)
+    ppca.μ = μ
+    # run E-step
+    E_z, E_zz = SSM.E_Step(ppca, X)
+    # check dimensions
+    @test size(E_z) == (100, 2)
+    @test size(E_zz) == (100, 2, 2)
+    # run M-step, but first save the old parameters
+    W_old = ppca.W
+    σ²_old = ppca.σ²
+    SSM.M_Step!(ppca, X, E_z, E_zz)
+    # check if the parameters are updated
+    @test ppca.W !== W_old
+    @test ppca.σ² !== σ²_old
+    @test ppca.μ === μ
+end
+
+function test_PPCA_fit()
+    # create ppca object
+    ppca = ProbabilisticPCA(;K=2, D=3)
+    # create data
+    X = randn(100, 3)
+    # fit the model
+    ll = fit!(ppca, X)
+    # check if the parameters are updated
+    @test ppca.σ² > 0
+    @test size(ppca.W) == (3, 2)
+    @test size(ppca.μ) == (1, 3)
+    @test size(ppca.z) == (100, 2)
+    # check loglikelihood only increases
+    @test all(diff(ll) .> 0)
+    # also check that the loglikelihood is a scalar
+    ll = SSM.loglikelihood(ppca, X)
+    @test size(ll) == ()
 end
 
 @testset "PPCA Tests" begin
     test_PPCA_with_params()
     test_PPCA_without_params()
+    test_PPCA_E_and_M_Step()
+    test_PPCA_fit()
 end
 
 """
@@ -988,7 +1034,6 @@ function test_HMMGLM_initialization()
     @test poisson_model.B[2].regression.λ == 0.0
 
 end
-
 
 @testset "SwitchingRegression Tests" begin
     test_HMMGLM_initialization()
