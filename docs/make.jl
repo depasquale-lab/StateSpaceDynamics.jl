7fd01c3519c7280574facd5ed5d3e603c32986d8
Carson Loughridge <crlough@bu.edu>
2024-05-30 16:35:47 -0400

    Remove debug print lines

--- docs/make.jl
+++ docs/make.jl
@@ -3,9 +3,7 @@ using Revise
 using SSM
 
 root = joinpath(dirname(pathof(SSM)), "..", "docs")
-println("Current root: ", root)
 
 entr([], [SSM]) do
-    println("Current working directory: ", pwd())
     makedocs(root = root, sitename="SSM Julia")
 end
